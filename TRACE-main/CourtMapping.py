902f4b19e946ab5a38384042405874c447df9d4e
Smaran Rangarajan <srangarajan455@student.fuhsd.org>
2025-06-11 15:56:52 -0700

    Updated Court Mapping, Finished

--- TRACE-main/CourtMapping.py
+++ TRACE-main/CourtMapping.py
@@ -2,7 +2,7 @@
 from numpy import float32, zeros,  uint8, shape, array, squeeze
 from cv2 import VideoCapture, getPerspectiveTransform, warpPerspective, line, circle, rectangle, perspectiveTransform
 from TraceHeader import videoFile, checkPath
-
+import cv2;
 video = VideoCapture(videoFile)
 checkPath(videoFile)
 frameWidth = int(video.get(3))
@@ -14,7 +14,7 @@ heightP = int(1585)
 width = int(967)
 height = int(1585)
 
-ratio = (1097/2377)
+ratio = 6.1/13.4
 courtHeight = int(height * 0.6)
 courtWidth = int(courtHeight * ratio)
 yOffset = int((height - courtHeight) / 2)
@@ -33,12 +33,26 @@ def courtMap(frame, top_left, top_right, bottom_left, bottom_right):
     return dst, M
 
 def showLines(frame):
-    rectangle(frame, (0,0),(width,height),(255,255,255),6)
-    rectangle(frame, (xOffset,yOffset), (courtWidth+xOffset,courtHeight+yOffset), (255, 255, 255), 2)
-    rectangle(frame, (xOffset,yOffset+int(courtHeight*0.5)), (courtWidth+xOffset,yOffset+int(courtHeight*0.5)), (255, 255, 255), 2)
-    rectangle(frame, (xOffset+int(courtWidth*0.124886),yOffset), (courtWidth+xOffset-int(courtWidth*0.124886),courtHeight+yOffset), (255, 255, 255), 2)
-    rectangle(frame, (xOffset+int(courtWidth*0.124886),yOffset+int(courtHeight*0.23054)), (courtWidth+xOffset-int(courtWidth*0.124886),courtHeight+yOffset-int(courtHeight*0.23054)), (255, 255, 255), 2)
-    rectangle(frame, (xOffset+int(courtWidth*0.5),yOffset+int(courtHeight*0.23054)), (courtWidth+xOffset-int(courtWidth*0.5),courtHeight+yOffset-int(courtHeight*0.23054)), (255, 255, 255), 2)
+    cv2.rectangle(frame, (xOffset,yOffset), (courtWidth+xOffset,courtHeight+yOffset), (255, 255, 255), 2)
+    centerX=xOffset+courtWidth//2
+    cv2.line(frame, (centerX, yOffset), (centerX, yOffset + courtHeight), (255, 255, 255), 2)
+   
+    shortServiceLineY1 = yOffset + int(courtHeight * 0.35472)  
+    cv2.line(frame, (xOffset, shortServiceLineY1), (xOffset + courtWidth, shortServiceLineY1), (255, 255, 255), 2)
+    shortServiceLineY2 = yOffset + int(courtHeight * 0.64528)  
+    cv2.line(frame, (xOffset, shortServiceLineY2), (xOffset + courtWidth, shortServiceLineY2), (255, 255, 255), 2)
+
+    singlesOffset = int(courtWidth * (.46/6.1)) 
+    leftSinglesX = xOffset + singlesOffset
+    rightSinglesX = xOffset + courtWidth - singlesOffset
+    cv2.line(frame, (leftSinglesX, yOffset), (leftSinglesX, yOffset + courtHeight), (255, 255, 255), 2)
+    cv2.line(frame, (rightSinglesX, yOffset), (rightSinglesX, yOffset + courtHeight), (255, 255, 255), 2)
+    backServiceLineY_top = yOffset + int(courtHeight * 0.0567)  
+    backServiceLineY_bottom = yOffset + int(courtHeight * 0.9424)  
+    cv2.line(frame, (leftSinglesX, backServiceLineY_top), (rightSinglesX, backServiceLineY_top), (255, 255, 255), 2)
+    cv2.line(frame, (leftSinglesX, backServiceLineY_bottom), (rightSinglesX, backServiceLineY_bottom), (255, 255, 255), 2)
+
+
     return frame
 
 def showPoint(frame, M, point):
